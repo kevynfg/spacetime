30df3b8406afba04d93433efbe82deb146629405
Kevyn Gonçalves <kevynredlotus@gmail.com>
2023-05-15 23:00:43 -0300

    initial commit 2

---
+++ README.md
@@ -0,0 +1,3 @@
+### Spacetime from NLW
+
+Project developed following the nlw from rocketseat to keep up to date with the javascript ecosystem.
---
+++ mobile/App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="bg-gray-950 flex-1 items-center justify-center">
+      <Text className="text-gray-50">Hello world</Text>
+      <StatusBar style="light" translucent/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
